79146d0a72ae1999e5eb3b571e653db9c0c8772d
kcshan <cshan.kong@gmail.com>
2022-10-22 06:13:52 +0800

    fix: update

---
+++ 1.1.CFP/1.1.ConFirstPro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1.ConFirstPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // 1. 单行注释
+            // Console.Clear(); // 清屏
+
+            // 2. 块注释
+            /*
+            Console.WriteLine("a");
+            string ss = Console.ReadLine();
+            Console.WriteLine(ss);
+            Console.WriteLine($"信息：{ss}");
+            Console.WriteLine("{0},欢迎来到xxx", ss);
+            */
+
+            // 关键词
+            int rowID = 123;
+            int a; // 声明变量
+            // = 赋值运算符
+            a = 12; // 初始化
+            a = rowID + 2;
+            byte bl = 255; // 256超范围
+
+            a = 23;
+            // a = 2147483648;
+            long longNum = 34567;
+            var money = 2.3; // double
+            float f1 = 3.45f;  // float
+
+
+
+            //Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
